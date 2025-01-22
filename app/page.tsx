6be3ed5859b36a74d12f7f0aca97aa5aa7d75f1e
Josh <joshuakibwage23@gmail.com>
2025-01-22 10:30:05 +0300

    Add navbar links and style

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-4xl font-bold text-fuchsia-600">Hello word</h1>
+    <h1 className="text-4xl">Hello word</h1>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,41 @@
+import { NAV_LINKS } from "@/constants"
+import Image from "next/image"
 import Link from "next/link"
+import Button from "./Button"
 
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5 ">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5 ">
         <Link href="/">
-
+            <Image src="/hilink-logo.svg" alt="logo" height={29} width={74} />
         </Link>
+        <ul className="hidden h-full gap-12 lg:flex">
+                {NAV_LINKS.map((link) => (
+                    <Link href={link.href} key={link.key}
+                    className="regular-16 text-gray-50 flex-center pb-1.5 transition-all hover:font-bold"
+                    >
+                        {link.label}
+                    </Link>
+                ))}
+        </ul>
+
+        <div className="lg:flexCenter hidden">
+            <Button
+                type="button"
+                title="Log In"
+                icon="/user.svg"
+                variant="btn_dark_green"
+            />
+        </div>
+
+        <img 
+            src="menu.svg"
+            alt="menu" 
+            className="lg:hidden cursor-pointer inline-block"
+            width={32}
+            height={32}
+        />
     </nav>
   )
 }
